af271d21ac0cdf117db6bf80f745526984696fb6
Andrey Makarov <andrew.r3nya@gmail.com>
2014-11-21 16:32:50 +0300

    added backend validation

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express		= require('express'),
 	path        = require('path'),
 	nodemailer  = require('nodemailer'),
+	validator	= require('validator'),
 	csrf        = require('csurf');
 
 var getHtmlMail = require('../lib/getHtmlMail');
@@ -24,23 +25,31 @@ router.route('/')
 	    });
 	})
 	.post(function (req, res) {
-	    getHtmlMail(req.body.name, req.body.email, req.body.body, function(htmlBody) {
-	        mailer.sendMail({
-	            from    : config.mailbot.from,
-	            to      : config.mailbot.to,
-	            subject : config.mailbot.subj,
-	            html    : htmlBody
-	        }, function (err, info) {
-	            if (err) {
-	                console.log('Mail error: ', err);
-	                res.status(500);
-	                res.send({title: 'Упс…', msg: 'Почта сломалась…', status: 'error'});
-	            } else {
-	                console.log('Message sent: ', info.response);
-	                res.send({title: 'Фух!', msg: 'Сообщение отправлено.', status: 'success'});
-	            }
-	        });
-	    });
+		if (validator.isEmail(req.body.email)) {
+			getHtmlMail(req.body.name, req.body.email, req.body.body, function (htmlBody) {
+				mailer.sendMail({
+					from: config.mailbot.from,
+					to: config.mailbot.to,
+					subject: config.mailbot.subj,
+					html: htmlBody
+				}, function (err, info) {
+					if (err) {
+						console.log('Mail error: ', err);
+						res.status(500);
+						res.send({title: 'Упс…', msg: 'Почта сломалась…', status: 'error'});
+					} else {
+						console.log('Message sent: ', info.response);
+						res.send({title: 'Фух!', msg: 'Сообщение отправлено.', status: 'success'});
+					}
+				});
+			});
+		} else if (!req.body.name) {
+			res.send({title: 'Упс…', msg: 'Анон не пройдет!', status: 'error'});
+		} else if (!req.body.body) {
+			res.send({title: 'Упс…', msg: 'Мне нравится когда мне что-то пишут…', status: 'error'});
+		} else {
+			res.send({title: 'Упс…', msg: 'Укажите корректную эл.почту', status: 'error'});
+		}
 });
 
 module.exports = router;
